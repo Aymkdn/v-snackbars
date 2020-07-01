e456cc6680aec6b54d8a9b0856b032c026b51b5c
Aymeric <Aymkdn@users.noreply.github.com>
2020-07-01 17:27:44 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # v-snackbars
 Display the `v-snackbar` (from Vuetify) with a stack display
+
+![Capture](https://user-images.githubusercontent.com/946315/86259071-d8468480-bbbb-11ea-9eca-3aac7d9be455.PNG)
+
+## Requirements
+
+[Vuetify](https://www.npmjs.com/package/vuetify) > v2.3 (it may work with an earlier version of Vuetify but I haven't tested)
+
+## Install
+
+```bash
+npm install v-snackbars
+```
+
+## Demo
+
+## How to use
+
+It will be very similar to the `v-snackbar` (you can use the same options):
+```vue
+<v-snackbars :messages.sync="messages" timeout="6000" color="blue" bottom right></v-snackbars>
+```
+
+You need to provide a `messages` array. Using a `push` on the array will cause the text to be shown in a snackbar.
+
+For example, to display "This is a message", just do the below:
+```javascript
+this.messages.push("This is a message");
+```
+
+A `close` button is used by default. If you prefer to define your own action button, you can use a ` v-slot:action`.
+For example:
+```vue
+<v-snackbars :messages.sync="messages" timeout="-1" color="black" top right>
+  <template v-slot:action="{ close, index }">
+    <v-btn text @click="close(index)">Dismiss</v-btn>
+  </template>
+</template>
+```
+
+By clicking on `Dismiss`, it will remove the related snackbar.
---
+++ v-snackbars.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <v-snackbar v-bind="$attrs" v-model="snackbar.show" :key="snackbar.key" :class="'v-snackbars v-snackbars-'+idx" :timeout="-1" v-for="(snackbar,idx) in snackbars">
+      {{ snackbar.message }}
+      <template v-slot:action>
+        <slot name="action" :close="removeMessage" :index="snackbar.key">
+          <v-btn text @click="removeMessage(snackbar.key)">close</v-btn>
+        </slot>
+      </template>
+    </v-snackbar>
+    <css-style>
+      .v-snackbars .v-snack__wrapper {
+        transition: {{topOrBottom}} 500ms;
+        {{topOrBottom}}: 0;
+      }
+    </css-style>
+    <css-style :key="idx" v-for="idx in len">
+      .v-snackbars-{{idx}} > .v-snack__wrapper {
+        {{topOrBottom}}:{{ idx*65 }}px;
+      }
+    </css-style>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'v-snackbars',
+  props:{
+    'messages':{
+      type:Array,
+      default:() => []
+    },
+    'timeout':{
+      type:[Number,String],
+      default:5000
+    }
+  },
+  data () {
+    return {
+      topOrBottom:'bottom',
+      len:0, // we need it to have a css transition
+      snackbars:[] // array of {key, message, show(true)}
+    }
+  },
+  components:{
+    'css-style': {
+      render: function (createElement) {
+        return createElement('style', this.$slots.default)
+      }
+    }
+  },
+  watch:{
+    messages () {
+      this.setSnackbars();
+    }
+  },
+  methods:{
+    setSnackbars () {
+      for (let i=this.snackbars.length; i<this.messages.length; i++) {
+        let key = i+'-'+Date.now();
+        this.snackbars.push({
+          key:key,
+          message:this.messages[i],
+          show:true
+        })
+        if (this.timeout > 0) {
+          setTimeout(() => this.removeMessage(key), this.timeout*1);
+        }
+      }
+      if (this.snackbars.length > this.len) this.len=this.snackbars.length;
+    },
+    removeMessage (key) {
+      let idx = this.snackbars.findIndex(s => s.key === key);
+      if (idx > -1) {
+        this.snackbars.splice(idx, 1);
+        this.$emit('update:messages', this.snackbars.map(o => o.message));
+      }
+    }
+  },
+  created () {
+    global.debug = this;
+    if (this.$attrs.top) this.topOrBottom='top';
+    this.setSnackbars();
+  }
+}
+</script>
